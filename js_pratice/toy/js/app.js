7918c80b79f53c8d497dfcfd4adb4105810f4c77
kys1651 <akys159357@naver.com>
2022-02-21 00:53:16 +0900

    toy_project(yet_calcul)

--- js_pratice/toy/js/app.js
+++ js_pratice/toy/js/app.js
@@ -1,53 +1,73 @@
 const select = document.querySelector("#year-select");
 const test = document.querySelector(".test");
 let selected;
+const button = document.createElement("button");
+button.innerText = "결과";
+button.classList.add("button");
 
 function creative(year){
    test.appendChild(check_class1(year));
    test.appendChild(check_class2(year));
    test.appendChild(check_class3(year));
+   test.appendChild(button);
 }
 
 function check_class1(year) {
     const div = document.createElement("div");
+    div.classList.add("class1");
+    div.classList.add("class_title");
+    div.appendChild(creative_h2("'함께형'교양 (총 필요 학점: 4)"));
+    
+    const form = document.createElement("form");
+    form.classList.add("class_form");
 
-    div.appendChild(creative_h2("'함께형'교양(총 필요 학점: 4)"));
-    div.appendChild(creative_checkbox("<신입생 세미나> (필요 이수 학점: 1)"));
+    form.appendChild(creative_checkbox("<신입생 세미나> (필요 이수 학점: 1)"));
     if(year==="18-19"||year=="20") {
-        div.appendChild(creative_checkbox("자아의 발견과 진로탐색1,2 (필요 이수 학점: 1)"));
-        div.appendChild(creative_checkbox("'자기계발과 사회봉사'영역 (필요 이수 학점: 2)"));
+        form.appendChild(creative_checkbox("자아의 발견과 진로탐색1,2 (필요 이수 학점: 1)"));
+        form.appendChild(creative_checkbox("'자기계발과 사회봉사'영역 (필요 이수 학점: 2)"));
     }else {
-        div.appendChild(creative_checkbox("'자기계발과 사회봉사'영역 (필요 이수 학점: 3)"));
+        form.appendChild(creative_checkbox("'자기계발과 사회봉사'영역 (필요 이수 학점: 3)"));
     }
+    div.appendChild(form);
     return div;
 }
 
 function check_class2(year){
     const div = document.createElement("div");
+    div.classList.add("class2");
+    div.classList.add("class_title");
     const a = document.createElement("div");
     
-    a.appendChild(creative_h2((year==="18-19"||year=="20")?"기본교육":"공통교양"));
+    a.appendChild(creative_h2((year==="18-19"||year=="20")?"기본교육 (필요 이수 학점: 14)":"공통교양(필요 이수 학점: 8)"));
     
-    a.appendChild(creative_checkbox("사고와 표현1 (필요 이수 학점: 2)"));
-    a.appendChild(creative_checkbox("사고와 표현2 (필요 이수 학점: 2)"));
-    a.appendChild(creative_checkbox(((year==="18-19"||year=="20")?"대학영어":"EAL" )+ "(필요 이수 학점: 2)"));
-    a.appendChild(creative_checkbox(((year==="18-19"||year=="20")?"생활영어1,2":"EGC" )+ "(필요 이수 학점: 2)"));
+    const form_a = document.createElement("form");
+    form_a.classList.add("class_form");
+
+    form_a.appendChild(creative_checkbox("사고와 표현1 (필요 이수 학점: 2)"));
+    form_a.appendChild(creative_checkbox("사고와 표현2 (필요 이수 학점: 2)"));
+    form_a.appendChild(creative_checkbox(((year==="18-19"||year=="20")?"대학영어":"EAL" )+ "(필요 이수 학점: 2)"));
+    form_a.appendChild(creative_checkbox(((year==="18-19"||year=="20")?"생활영어1,2":"EGC" )+ "(필요 이수 학점: 2)"));
 
     const b = document.createElement("div");
+    const form_b = document.createElement("form");
+    form_b.classList.add("class_form");
 
-    b.appendChild(creative_h2((year==="18-19"||year=="20")?"기초교육":"기초교양"));
+    b.appendChild(creative_h2((year==="18-19"||year=="20")?"기초교육 (계열별로 상이)":"기초교양 (계열별로 상이)"));
 
     if(year==="18-19"||year=="20") {
-        a.appendChild(creative_checkbox("'고전 및 외국어'영역 (필요 이수 학점: 3)"));
-        a.appendChild(creative_checkbox("'정보이해'영역 (필요 이수 학점: 3)"));
+        form_a.appendChild(creative_checkbox("'고전 및 외국어'영역 (필요 이수 학점: 3)"));
+        form_a.appendChild(creative_checkbox("'정보이해'영역 (필요 이수 학점: 3)"));
     }else {
-        b.appendChild(creative_checkbox("'고전'영역 (필요 이수 학점: 3)"));
-        b.appendChild(creative_checkbox("'글로벌'영역 (필요 이수 학점: 3)"));
-        b.appendChild(creative_checkbox("'정보(SW.AI)'영역 (필요 이수 학점: 3)"));
+        form_b.appendChild(creative_checkbox("'고전'영역 (필요 이수 학점: 3)"));
+        form_b.appendChild(creative_checkbox("'글로벌'영역 (필요 이수 학점: 3)"));
+        form_b.appendChild(creative_checkbox("'정보(SW.AI)'영역 (필요 이수 학점: 3)"));
     }
-    b.appendChild(creative_checkbox("'기초인문사회과학'영역 (필요 이수 학점: (계열별 상이))"));
-    b.appendChild(creative_checkbox("'기초자연과학'영역 (필요 이수 학점: (계열별 상이))"));
+    form_b.appendChild(creative_checkbox("'기초인문사회과학'영역 (필요 이수 학점: (계열별 상이))"));
+    form_b.appendChild(creative_checkbox("'기초자연과학'영역 (필요 이수 학점: (계열별 상이))"));
     
+    a.appendChild(form_a);
+    b.appendChild(form_b);
+
     div.appendChild(a);
     div.appendChild(b);
     return div;
@@ -55,18 +75,25 @@ function check_class2(year){
 
 function check_class3(year){
     const div = document.createElement("div");
-    div.appendChild(creative_h2((year==="18-19"||year=="20")?"핵심교양":"균형교양"));
+    div.classList.add("class3");
+    div.classList.add("class_title");
 
-    div.appendChild(creative_checkbox("'인간과 논리'영역(필요 이수 학점: 3)"));
-    div.appendChild(creative_checkbox("'문화과 예술'영역(필요 이수 학점: 3)"));
-    div.appendChild(creative_checkbox("'사회와 역사'영역(필요 이수 학점: 3)"));
-    div.appendChild(creative_checkbox("'자연과 과학'영역(필요 이수 학점: 3)"));
+    div.appendChild(creative_h2(((year==="18-19"||year=="20")?"핵심교양":"균형교양")+" (필요 이수 학점: 12학점)"));
+
+    const form = document.createElement("form");
+    form.classList.add("class_form");
+
+    form.appendChild(creative_checkbox("'인간과 논리'영역(필요 이수 학점: 3)"));
+    form.appendChild(creative_checkbox("'문화과 예술'영역(필요 이수 학점: 3)"));
+    form.appendChild(creative_checkbox("'사회와 역사'영역(필요 이수 학점: 3)"));
+    form.appendChild(creative_checkbox("'자연과 과학'영역(필요 이수 학점: 3)"));
     
+    div.appendChild(form);
     return div;
 }
 
 function creative_checkbox(text) {
-    const label = document.createElement("lable");
+    const label = document.createElement("label");
     label.innerText = text;
     const check = document.createElement("input");
     check.type = "checkbox";
